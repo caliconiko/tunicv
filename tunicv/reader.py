f2f9ca9527e26ef1e569a98f1001e02f17cc8ff4
PhotonNikko <69907559+photon-niko@users.noreply.github.com>
2022-04-29 11:37:33 +0700

    tidying up

--- tunicv/reader.py
+++ tunicv/reader.py
@@ -9,41 +9,50 @@ class Reader:
         self.image = cv2.imread(str(self.path))
 
     def read(self):
+        # gray scale it
         gray = cv2.cvtColor(self.image, cv2.COLOR_RGB2GRAY)
+        # get only the writing
         _, stroke = cv2.threshold(gray, 254, 255, cv2.THRESH_OTSU)
+        # get stroke width of diffrent parts of image
         distance_transformed = cv2.distanceTransform(stroke, cv2.DIST_L2, 0)
-
+        # get the max stroke width
         max_stroke = np.max(distance_transformed)
-
+        # make the writing as thin as possible without destroying it
         eroded_stroke = Reader.morph_func(stroke, cv2.erode, int(max_stroke/2))
 
-        # double stroke dimensions
-        double_stroke = cv2.resize(eroded_stroke, (0, 0), fx=2, fy=2)
+        RESCALE_FACTOR = 2
+        # make image biggar
+        double_stroke = cv2.resize(eroded_stroke, (0, 0), fx=RESCALE_FACTOR, fy=RESCALE_FACTOR)
 
+        # skelotonize to make it even thinner
         skeletonized = Reader.skeletonize(double_stroke)
-        # threshold again
+        # threshold again just in case
         _, skeletonized = cv2.threshold(skeletonized, 254, 255, cv2.THRESH_OTSU)
-        # open skeleton
-        opened_skeleton = Reader.morph(skeletonized, kernel_size=2, morph=cv2.MORPH_OPEN)
-        # close skeleton
+        # get rid of holes
         closed_skeleton = Reader.morph(skeletonized, kernel_size=5, morph=cv2.MORPH_CLOSE)
         
+        # squish the image horizontally to find the lines
         hor_avg = cv2.normalize(np.average(closed_skeleton, axis=1), None, 0, 1, cv2.NORM_MINMAX)
-        # threshold hor_sum
-        _, hor_sum_thresh = cv2.threshold(hor_avg, 0.5, 1, cv2.THRESH_BINARY)
-        # pad hor_sum_thresh
-        hor_sum_pad = np.pad(np.array(hor_sum_thresh, dtype=np.uint8), ((1, 1), (1, 1)), 'constant', constant_values=0)
+        # binarize it just in case
+        _, hor_avg_thresh = cv2.threshold(hor_avg, 0.5, 1, cv2.THRESH_BINARY)
+        # pad margins to make the contours detectable
+        hor_avg_pad = np.pad(np.array(hor_avg_thresh, dtype=np.uint8), ((1, 1), (1, 1)), 'constant', constant_values=0)
         # find contours
-        contours, _ = Reader.find_contours(hor_sum_pad)
+        contours, _ = Reader.find_contours(hor_avg_pad)
+
+        line_ys = []
         for contour in contours:
-            # get bounding box
-            x, y, w, h = cv2.boundingRect(contour)
-            print(y+h/2)
-        
-        thick_hor_sum = cv2.resize(hor_sum_pad, (0, 0), fx=200, fy=1)
+            # get center of contour
+            center = np.average(contour, axis=0)
+            # store y part of center
+            line_ys.append(center[1])
+            
+        # thick version of line finding to make it human visible
+        thick_hor_avg = cv2.resize(hor_avg_thresh, (0, 0), fx=200, fy=1)
 
         print(np.where(hor_avg > 0.5))
-        cv2.imshow("Image", hor_sum_pad)
+        cv2.imshow("image", thick_hor_avg)
+        cv2.imshow("image1", closed_skeleton)
         cv2.waitKey(0)
 
     # hat tip to https://gist.github.com/jsheedy/3913ab49d344fac4d02bcc887ba4277d
